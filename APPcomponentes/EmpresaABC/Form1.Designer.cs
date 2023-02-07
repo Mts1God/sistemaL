8982497bccee83ded2d526ffde31d9517fcb56fa
MATEUS FELIPE RIBEIRO <mateus.fribeiro1@senacsp.edu.br>
2023-02-07 17:16:04 -0300

    sistema

---
+++ APPcomponentes/EmpresaABC/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace EmpresaABC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.ckblivro = new System.Windows.Forms.CheckBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.mskCPF = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lstlistagem = new System.Windows.Forms.ListBox();
+            this.rdbMasculino = new System.Windows.Forms.RadioButton();
+            this.cbblista = new System.Windows.Forms.ComboBox();
+            this.pctimagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctimagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(135, 362);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // ckblivro
+            // 
+            this.ckblivro.AutoSize = true;
+            this.ckblivro.Location = new System.Drawing.Point(135, 290);
+            this.ckblivro.Name = "ckblivro";
+            this.ckblivro.Size = new System.Drawing.Size(45, 17);
+            this.ckblivro.TabIndex = 1;
+            this.ckblivro.Text = "livro";
+            this.ckblivro.UseVisualStyleBackColor = true;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(137, 64);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(81, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Insira seu nome";
+            // 
+            // mskCPF
+            // 
+            this.mskCPF.Location = new System.Drawing.Point(140, 88);
+            this.mskCPF.Name = "mskCPF";
+            this.mskCPF.Size = new System.Drawing.Size(100, 20);
+            this.mskCPF.TabIndex = 3;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(140, 127);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // lstlistagem
+            // 
+            this.lstlistagem.FormattingEnabled = true;
+            this.lstlistagem.Location = new System.Drawing.Point(135, 166);
+            this.lstlistagem.Name = "lstlistagem";
+            this.lstlistagem.Size = new System.Drawing.Size(120, 95);
+            this.lstlistagem.TabIndex = 5;
+            // 
+            // rdbMasculino
+            // 
+            this.rdbMasculino.AutoSize = true;
+            this.rdbMasculino.Location = new System.Drawing.Point(135, 325);
+            this.rdbMasculino.Name = "rdbMasculino";
+            this.rdbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rdbMasculino.TabIndex = 6;
+            this.rdbMasculino.TabStop = true;
+            this.rdbMasculino.Text = "Masculino";
+            this.rdbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // cbblista
+            // 
+            this.cbblista.FormattingEnabled = true;
+            this.cbblista.Location = new System.Drawing.Point(342, 88);
+            this.cbblista.Name = "cbblista";
+            this.cbblista.Size = new System.Drawing.Size(121, 21);
+            this.cbblista.TabIndex = 7;
+            // 
+            // pctimagem
+            // 
+            this.pctimagem.Location = new System.Drawing.Point(342, 166);
+            this.pctimagem.Name = "pctimagem";
+            this.pctimagem.Size = new System.Drawing.Size(121, 95);
+            this.pctimagem.TabIndex = 8;
+            this.pctimagem.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pctimagem);
+            this.Controls.Add(this.cbblista);
+            this.Controls.Add(this.rdbMasculino);
+            this.Controls.Add(this.lstlistagem);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.mskCPF);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.ckblivro);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctimagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.CheckBox ckblivro;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox mskCPF;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ListBox lstlistagem;
+        private System.Windows.Forms.RadioButton rdbMasculino;
+        private System.Windows.Forms.ComboBox cbblista;
+        private System.Windows.Forms.PictureBox pctimagem;
+    }
+}
+
---
+++ EmpresaABC/ConsultorioMedico/frmSplash.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace ConsultorioMedico
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.lblPorcentagem = new System.Windows.Forms.Label();
+            this.pgbCarregando = new System.Windows.Forms.ProgressBar();
+            this.lblcarregando = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctLogo
+            // 
+            this.pctLogo.Image = ((System.Drawing.Image)(resources.GetObject("pctLogo.Image")));
+            this.pctLogo.Location = new System.Drawing.Point(243, 81);
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.Size = new System.Drawing.Size(149, 120);
+            this.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctLogo.TabIndex = 0;
+            this.pctLogo.TabStop = false;
+            // 
+            // lblPorcentagem
+            // 
+            this.lblPorcentagem.AutoSize = true;
+            this.lblPorcentagem.Location = new System.Drawing.Point(307, 227);
+            this.lblPorcentagem.Name = "lblPorcentagem";
+            this.lblPorcentagem.Size = new System.Drawing.Size(21, 13);
+            this.lblPorcentagem.TabIndex = 1;
+            this.lblPorcentagem.Text = "0%";
+            // 
+            // pgbCarregando
+            // 
+            this.pgbCarregando.Location = new System.Drawing.Point(243, 265);
+            this.pgbCarregando.Name = "pgbCarregando";
+            this.pgbCarregando.Size = new System.Drawing.Size(149, 23);
+            this.pgbCarregando.TabIndex = 2;
+            // 
+            // lblcarregando
+            // 
+            this.lblcarregando.AutoSize = true;
+            this.lblcarregando.Location = new System.Drawing.Point(282, 308);
+            this.lblcarregando.Name = "lblcarregando";
+            this.lblcarregando.Size = new System.Drawing.Size(71, 13);
+            this.lblcarregando.TabIndex = 3;
+            this.lblcarregando.Text = "Carregando...";
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 392);
+            this.Controls.Add(this.lblcarregando);
+            this.Controls.Add(this.pgbCarregando);
+            this.Controls.Add(this.lblPorcentagem);
+            this.Controls.Add(this.pctLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmSplash";
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Label lblPorcentagem;
+        private System.Windows.Forms.ProgressBar pgbCarregando;
+        private System.Windows.Forms.Label lblcarregando;
+    }
+}
